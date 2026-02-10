9f3592dbf58a19522eb17c8f54c527d4fbd48870
Navaneethakrishnan Vijaykumar <navaneethakrishnan.v@amshuhu.com>
2026-02-10 14:11:55 +0530

    db connect using python
    Connecting MySQL database using Python

---
+++ db_task/config/config.py
@@ -0,0 +1,10 @@
+# MySQL Database Configuration
+# Update these values with your MySQL credentials
+
+DB_CONFIG = {
+    "host": "localhost",
+    "username": "root",
+    "password": "root",
+    "database": "navanee_sample_db",
+    "port": 3306
+}
